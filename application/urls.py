26bfc7e7eae2aa5f1d5151423b6df86968946e70
DanielfAlves27 <dfa2@cesar.school>
2025-04-03 19:56:46 -0300

    Implementacao do css e criacao do static

--- application/urls.py
+++ application/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'application'
-
 urlpatterns = [
     path('', views.home, name='home'),
+    path('cadastro/', views.cadastro, name='cadastro'), 
 ]
--- application/views.py
+++ application/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'application/home.html')
+
+def cadastro(request):
+    return render(request, 'application/cadastro.html')
